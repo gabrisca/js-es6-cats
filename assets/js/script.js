752f8a570bfca71bca71902a66ed1f61a7514d14
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-30 16:38:07 +0200

    milestone 1 ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,79 @@
 
 // Milestone 3
 // Creare un nuovo array con prima tutti i gattini femmina e poi tutti i gattini maschio, inserendo solamente nome e colore e colore e opacità del fiocco per ogni gatto.
-$(function(){
-  console.log("doc ready")
-});
 
 /////////////////////
 //F U N C T I O N S//
 /////////////////////
+const printHtml = (target, color, name) => {
+  return $(target).append(`
+  <li>
+        <i class="fas fa-cat fa-2x" style ="color:${color}"></i>
+        <span>${name}</span>
+  </li>
+  `)
+}
+
+////////E N D/////////
+//F U N C T I O N S//
+/////////////////////
+
+$(function(){
+  // console.log("doc ready");
+
+  const cats = [
+    {
+      name: "Birillo",
+      age: 15,
+      color: "#8B4513",
+      sex: "male"
+    },
+    {
+      name: "Gina",
+      age: 7,
+      color: "#000000",
+      sex: "female"
+    },
+    {
+      name: "Paul",
+      age: 2,
+      color: "#00BFFF",
+      sex: "male"
+    },
+    {
+      name: "Fragola",
+      age: 4,
+      color: "#F0FFFF",
+      sex: "male"
+    },
+    {
+      name: "Sansone",
+      age: 15,
+      color: "#A52A2A",
+      sex: "male"
+    },
+  ];
+
+  // Milestone 1: ciclo forEach
+  // LO UTILIZZO PER VEDERE I CONTENUTI DI UN ARRAY
+
+  cats.forEach((cat) => {
+    //cat corrisponde a cats[i]
+    console.log(cat); // mi restituisce gli oggetti (cat)dell'array (cats)
+    // stampo i gatti nell'ul grazie alla funzione printHtml
+    printHtml("ul.cats", cat.color, cat.name)
+    // senza return
+  });
+
+  // aggiungo e rimuovo classe active (effetto hover) --- per allenamento ---
+  $(".fa-cat").mouseenter(function (){
+    $(this).addClass("active");
+  });
+  $(".fa-cat").mouseleave(function (){
+    $(this).removeClass("active");
+  });
+  
+ 
+
+}); // END DOC.READY
+
